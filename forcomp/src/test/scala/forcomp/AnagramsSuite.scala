ed22258f07e678ba9b6f7b68c695295194e72ac8
davidalk <david.alkanani@gmail.com>
2013-10-27 03:13:33 +0000

    combinations

--- forcomp/src/test/scala/forcomp/AnagramsSuite.scala
+++ forcomp/src/test/scala/forcomp/AnagramsSuite.scala
@@ -41,14 +41,12 @@ class AnagramsSuite extends FunSuite {
   }
 
 
-
   test("subtract: lard - r") {
     val lard = List(('a', 1), ('d', 1), ('l', 1), ('r', 1))
     val r = List(('r', 1))
     val lad = List(('a', 1), ('d', 1), ('l', 1))
     assert(subtract(lard, r) === lad)
   }
-
 
 
   test("combinations: []") {
